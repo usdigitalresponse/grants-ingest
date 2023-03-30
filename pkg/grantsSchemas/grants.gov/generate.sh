890b9c6749d4d26f76b96f08504d75b4961227f4
TylerHendrickson <hendrickson.tsh@gmail.com>
2023-03-29 19:39:09 -0500

    Rename generated go file

--- pkg/grantsSchemas/grants.gov/generate.sh
+++ pkg/grantsSchemas/grants.gov/generate.sh
@@ -1,3 +1,3 @@
 wget -q -O schema.xsd https://apply07.grants.gov/apply/system/schemas/OpportunityDetail-V1.0.xsd &&\
-xsdgen -o generated_types.go -pkg grantsgov schema.xsd &&\
-sed -i '' -e 's?http://apply.grants.gov/system/OpportunityDetail-V1.0\ ??g' generated_types.go
+xsdgen -o types.go -pkg grantsgov schema.xsd &&\
+sed -i '' -e 's?http://apply.grants.gov/system/OpportunityDetail-V1.0\ ??g' types.go
