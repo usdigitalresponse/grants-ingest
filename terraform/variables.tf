f0bd8056f8c6ae696e2e2a97517bfd8919c250fb
Aaron Smith <slapula@users.noreply.github.com>
2023-04-28 16:07:48 -0500

    fix: localstack doesn't support active rule set resource yet (#69)
    * fix: localstack doesn't support active rule set resource yet
    
    * forgot to update desc
    
    * Add newline to end of file
    
    ---------
    
    Co-authored-by: Tyler Hendrickson <hendrickson.tsh@gmail.com>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -188,3 +188,9 @@ variable "ffis_ingest_email_address" {
   description = "Email address used to receive FFIS digests and save them to S3"
   default     = "ffis-ingest@grants.usdigitalresponse.org"
 }
+
+variable "ses_active_receipt_rule_set_enabled" {
+  description = "If false, prevents SES receipt rule set from being set to active. This should only be false in local development."
+  type        = bool
+  default     = true
+}
