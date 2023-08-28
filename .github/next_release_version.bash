11736e871817d37c42a4f510d67e3348fa312e3f
TylerHendrickson <hendrickson.tsh@gmail.com>
2023-08-28 20:58:24 +0000

    Add bash script to determine version for release-drafter

---
+++ .github/next_release_version.bash
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# Defaults
+next_version_release_year=$(TZ='UTC' date '+%Y')
+next_version_release_number=1
+
+# Ensure tag history is available
+git fetch --prune --unshallow
+
+tag=$(git describe --tags --match='release/[0-9][0-9][0-9][0-9].[0-9]*' refs/heads/main)
+regex='release\/([0-9]{4})\.([0-9]{0,})'
+if [[ $tag =~ $regex ]]; then
+    echo "Found tag for previous release: $tag"
+    prev_version_release_number="${BASH_REMATCH[2]}"
+    echo "Previous version number: $prev_version_release_number"
+    ((next_version_release_number=prev_version_release_number+1))
+else
+    echo "Could not locate a previous release version" >&2
+fi
+
+next_version="$next_version_release_year.$next_version_release_number"
+echo "Next version: $next_version"
+echo "result=$next_version" >> $GITHUB_OUTPUT
