640ea460617428a45c82d2fde0a76ff4b19c5126
Jeff Mohan <jeffsmohan@gmail.com>
2024-06-20 12:07:20 -0700

    Restore previous PersistGrantsGovXMLDB lambda function
    Having the two lambdas deployed alongside each other for now assures
    we won't drop any events for S3 objects in the old location. Eventually
    we can remove the old v2.xml version of the lambda.

--- terraform/main.tf
+++ terraform/main.tf
@@ -441,6 +441,12 @@ resource "aws_s3_bucket_notification" "grant_prepared_data" {
   lambda_function {
     lambda_function_arn = module.PersistGrantsGovXMLDB.lambda_function_arn
     events              = ["s3:ObjectCreated:*"]
+    filter_suffix       = "/grants.gov/v2.xml"
+  }
+
+  lambda_function {
+    lambda_function_arn = module.PersistGrantsGovXMLDB.lambda_function_arn
+    events              = ["s3:ObjectCreated:*"]
     filter_suffix       = "/grants.gov/v2.OpportunitySynopsisDetail_1_0.xml"
   }
 
