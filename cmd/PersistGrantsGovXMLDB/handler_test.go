587f86555b69aee6431ae1abf1b00feed549d6a6
tyler <1851017+TylerHendrickson@users.noreply.github.com>
2024-10-05 00:15:48 +0000

    Updated tests for handler.go

--- cmd/PersistGrantsGovXMLDB/handler_test.go
+++ cmd/PersistGrantsGovXMLDB/handler_test.go
@@ -5,8 +5,10 @@ import (
 	"context"
 	"crypto/tls"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"text/template"
 	"time"
@@ -16,6 +18,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -87,7 +90,7 @@ const SOURCE_OPPORTUNITY_TEMPLATE = `
 	<OpportunityTitle>Fun Grant</OpportunityTitle>
 	<OpportunityNumber>ABCD-1234</OpportunityNumber>
 	<OpportunityCategory>Some Category</OpportunityCategory>
-	<FundingInstrumentType>Clarinet</FundingInstrumentType>
+	<FundingInstrumentType>CA</FundingInstrumentType>
 	<CategoryOfFundingActivity>My Funding Category</CategoryOfFundingActivity>
 	<CategoryExplanation>Meow meow meow</CategoryExplanation>
 	<CFDANumbers>1234.567</CFDANumbers>
@@ -112,6 +115,52 @@ const SOURCE_OPPORTUNITY_TEMPLATE = `
 </OpportunitySynopsisDetail_1_0>
 `
 
+const SOURCE_FORECAST_TEMPLATE = `
+<OpportunityForecastDetail_1_0>
+	<OpportunityID>{{.OpportunityID}}</OpportunityID>
+	<OpportunityTitle>Fun Grant</OpportunityTitle>
+	<OpportunityNumber>ABCD-1234</OpportunityNumber>
+	<OpportunityCategory>D</OpportunityCategory>
+	<FundingInstrumentType>CA</FundingInstrumentType>
+	<CategoryOfFundingActivity>My Funding Category</CategoryOfFundingActivity>
+	<CFDANumbers>93.322</CFDANumbers>
+	<EligibleApplicants>12</EligibleApplicants>
+	<EligibleApplicants>13</EligibleApplicants>
+	<EligibleApplicants>22</EligibleApplicants>
+	<AdditionalInformationOnEligibility>This is some additional information on eligibility.</AdditionalInformationOnEligibility>
+	<AgencyCode>TEST-AC</AgencyCode>
+	<AgencyName>Bureau of Testing</AgencyName>
+	<PostDate>09082029</PostDate>
+	<LastUpdatedDate>{{.LastUpdatedDate}}</LastUpdatedDate>
+	<EstimatedSynopsisPostDate>09082029</EstimatedSynopsisPostDate>
+	<FiscalYear>2029</FiscalYear>
+	<EstimatedSynopsisCloseDate>09082030</EstimatedSynopsisCloseDate>
+	<EstimatedSynopsisCloseDateExplanation>Electronically submitted applications must be submitted no later than 11:59 p.m., ET, on the listed application due date.</EstimatedSynopsisCloseDateExplanation>
+	<EstimatedAwardDate>09082035</EstimatedAwardDate>
+	<EstimatedProjectStartDate>09082031</EstimatedProjectStartDate>
+	<AwardCeiling>0</AwardCeiling>
+	<AwardFloor>0</AwardFloor>
+	<EstimatedTotalProgramFunding>60000</EstimatedTotalProgramFunding>
+	<ExpectedNumberOfAwards>10</ExpectedNumberOfAwards>
+	<Description>Here is a description.</Description>
+	<Version>Forecast 1</Version>
+	<CostSharingOrMatchingRequirement>No</CostSharingOrMatchingRequirement>
+	<ArchiveDate>09092030</ArchiveDate>
+	<GrantorContactEmail>test@example.gov</GrantorContactEmail>
+	<GrantorContactEmailDescription>Inquiries</GrantorContactEmailDescription>
+	<GrantorContactName>Tester Person, Bureau of Testing, Office of Stuff &amp;lt;br/&amp;gt;</GrantorContactName>
+	<GrantorContactPhoneNumber>(555) 555-1234</GrantorContactPhoneNumber>
+</OpportunityForecastDetail_1_0>
+`
+
+var (
+	opportunityTemplate = template.Must(
+		template.New("xml").Delims("{{", "}}").Parse(SOURCE_OPPORTUNITY_TEMPLATE))
+	forecastTemplate = template.Must(
+		template.New("xml").Delims("{{", "}}").Parse(SOURCE_FORECAST_TEMPLATE),
+	)
+)
+
 func TestLambdaInvocationScenarios(t *testing.T) {
 	t.Run("Missing source object", func(t *testing.T) {
 		setupLambdaEnvForTesting(t)
@@ -119,12 +168,9 @@ func TestLambdaInvocationScenarios(t *testing.T) {
 		sourceBucketName := "test-source-bucket"
 		s3Client, err := setupS3ForTesting(t, sourceBucketName)
 		require.NoError(t, err)
-		sourceTemplate := template.Must(
-			template.New("xml").Delims("{{", "}}").Parse(SOURCE_OPPORTUNITY_TEMPLATE),
-		)
 		var sourceData bytes.Buffer
 		require.NoError(t, err)
-		require.NoError(t, sourceTemplate.Execute(&sourceData, map[string]string{
+		require.NoError(t, opportunityTemplate.Execute(&sourceData, map[string]string{
 			"OpportunityID":   "123456",
 			"LastUpdatedDate": "01022023",
 		}))
@@ -162,6 +208,37 @@ func TestLambdaInvocationScenarios(t *testing.T) {
 		}
 	})
 
+	t.Run("Empty XML stream", func(t *testing.T) {
+		setupLambdaEnvForTesting(t)
+
+		sourceBucketName := "test-source-bucket"
+		s3Client, err := setupS3ForTesting(t, sourceBucketName)
+		require.NoError(t, err)
+		require.NoError(t, err)
+		_, err = s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
+			Bucket: aws.String(sourceBucketName),
+			Key:    aws.String("123/123456/grants.gov/v2.xml"),
+			Body:   bytes.NewReader([]byte{}),
+		})
+		require.NoError(t, err)
+		dynamodbClient := mockDynamoDBUpdateItemAPI{
+			mockUpdateItemAPI(func(context.Context, *dynamodb.UpdateItemInput, ...func(*dynamodb.Options)) (*dynamodb.UpdateItemOutput, error) {
+				t.Helper()
+				require.Fail(t, "UpdateItem called unexpectedly")
+				return nil, nil
+			}),
+		}
+		err = handleS3EventWithConfig(s3Client, dynamodbClient, context.TODO(), events.S3Event{
+			Records: []events.S3EventRecord{
+				{S3: events.S3Entity{
+					Bucket: events.S3Bucket{Name: sourceBucketName},
+					Object: events.S3Object{Key: "123/123456/grants.gov/v2.xml"},
+				}},
+			},
+		})
+		assert.ErrorIs(t, err, io.EOF)
+	})
+
 	t.Run("Context canceled during invocation", func(t *testing.T) {
 		setupLambdaEnvForTesting(t)
 		s3Client, err := setupS3ForTesting(t, "source-bucket")
@@ -195,9 +272,171 @@ func TestLambdaInvocationScenarios(t *testing.T) {
 			require.Fail(t, "Invocation error could not be interpreted as *multierror.Error")
 		}
 	})
+
+	t.Run("Decodes and persists", func(t *testing.T) {
+		for _, tt := range []struct {
+			grantRecordType string
+			xmlTemplate     *template.Template
+		}{
+			{"opportunity", opportunityTemplate},
+			{"forecast", forecastTemplate},
+		} {
+			t.Run(tt.grantRecordType, func(t *testing.T) {
+
+				setupLambdaEnvForTesting(t)
+
+				sourceBucketName := "test-source-bucket"
+				s3Client, err := setupS3ForTesting(t, sourceBucketName)
+				require.NoError(t, err)
+				var sourceData bytes.Buffer
+				require.NoError(t, err)
+				require.NoError(t, tt.xmlTemplate.Execute(&sourceData, map[string]string{
+					"OpportunityID":   "123456",
+					"LastUpdatedDate": "01022023",
+				}))
+				require.NoError(t, err)
+				_, err = s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
+					Bucket: aws.String(sourceBucketName),
+					Key:    aws.String("123/123456/grants.gov/v2.xml"),
+					Body:   bytes.NewReader(sourceData.Bytes()),
+				})
+				require.NoError(t, err)
+				dynamodbClient := mockDynamoDBUpdateItemAPI{
+					mockUpdateItemAPI(func(ctx context.Context, params *dynamodb.UpdateItemInput, opts ...func(*dynamodb.Options)) (*dynamodb.UpdateItemOutput, error) {
+						t.Helper()
+
+						// Check expected item key
+						var itemKey string
+						assert.NoError(t, attributevalue.Unmarshal(params.Key["grant_id"], &itemKey))
+						assert.Equal(t, "123456", itemKey)
+
+						// Check expected `is_forecast` attribute value
+						foundTestValue := false
+						for attrNamePlaceholder, attrName := range params.ExpressionAttributeNames {
+							if attrName == "is_forecast" {
+								// DynamoDB maps attribute names and corresponding values to placeholders.
+								// e.g. `{"#13": "is_forecast"}` corresponds to `{":13": true}`
+								// means `is_forecast = true`.
+								// Note that the placeholder numbers are assigned in arbitrary order.
+								attrValuePlaceholder := strings.Replace(attrNamePlaceholder, "#", ":", 1)
+								attrValue := params.ExpressionAttributeValues[attrValuePlaceholder]
+								var itemIsForecast bool
+								assert.NoError(t, attributevalue.Unmarshal(attrValue, &itemIsForecast))
+								if tt.grantRecordType == "opportunity" {
+									assert.False(t, itemIsForecast)
+									foundTestValue = true
+								} else if tt.grantRecordType == "forecast" {
+									assert.True(t, itemIsForecast)
+									foundTestValue = true
+								} else {
+									require.Fail(t, "Cannot test an unrecognized grantRecord type")
+								}
+								break
+							}
+						}
+						if !foundTestValue {
+							require.Fail(t,
+								"Failed to locate `is_forecast` attribute in DynamoDB item",
+								"Cross-checked DynamoDB item attribute value placeholders map "+
+									"against the following name placeholders map: %v",
+								params.ExpressionAttributeNames)
+						}
+						return nil, nil
+					}),
+				}
+				err = handleS3EventWithConfig(s3Client, dynamodbClient, context.TODO(), events.S3Event{
+					Records: []events.S3EventRecord{{S3: events.S3Entity{
+						Bucket: events.S3Bucket{Name: sourceBucketName},
+						Object: events.S3Object{Key: "123/123456/grants.gov/v2.xml"},
+					}}},
+				})
+				require.NoError(t, err)
+			})
+		}
+	})
 }
 
-func TestProcessOpportunity(t *testing.T) {
+func TestDecodeNextGrantRecord(t *testing.T) {
+	getOpportunityXML := func(OpportunityID, LastUpdatedDate string) *bytes.Buffer {
+		t.Helper()
+		data := &bytes.Buffer{}
+		require.NoError(t, opportunityTemplate.Execute(data, map[string]string{
+			"OpportunityID":   OpportunityID,
+			"LastUpdatedDate": LastUpdatedDate,
+		}), "Unexpected error generating opportunity XML data during test setup")
+		return data
+	}
+	getForecastXML := func(OpportunityID, LastUpdatedDate string) *bytes.Buffer {
+		t.Helper()
+		data := &bytes.Buffer{}
+		require.NoError(t, forecastTemplate.Execute(data, map[string]string{
+			"OpportunityID":   OpportunityID,
+			"LastUpdatedDate": LastUpdatedDate,
+		}), "Unexpected error generating forecast XML data during test setup")
+		return data
+	}
+	testDateString := time.Now().Format(grantsgov.TimeLayoutMMDDYYYYType)
+
+	t.Run("Decodes opportunity grantRecord from XML", func(t *testing.T) {
+		o, err := decodeNextGrantRecord(getOpportunityXML("12345", testDateString))
+		assert.NoError(t, err)
+		if assert.IsType(t, opportunity{}, o) {
+			assert.Equal(t, "12345", string(o.(opportunity).OpportunityID),
+				"opportunity has unexpected OpportunityID")
+		}
+	})
+
+	t.Run("Decodes forecast grantRecord from XML", func(t *testing.T) {
+		f, err := decodeNextGrantRecord(getForecastXML("12345", testDateString))
+		assert.NoError(t, err)
+		if assert.IsType(t, forecast{}, f) {
+			assert.Equal(t, "12345", string(f.(forecast).OpportunityID),
+				"forecast has unexpected OpportunityID")
+		}
+	})
+
+	t.Run("Stops reading after decoding next grantRecord or EOF", func(t *testing.T) {
+		r := io.MultiReader(
+			getOpportunityXML("12345", testDateString),
+			getForecastXML("56789", testDateString),
+			getForecastXML("13579", testDateString),
+			getOpportunityXML("24680", testDateString),
+		)
+		o1, err := decodeNextGrantRecord(r)
+		assert.NoError(t, err)
+		if assert.IsType(t, opportunity{}, o1) {
+			assert.Equal(t, "12345", string(o1.(opportunity).OpportunityID),
+				"opportunity has unexpected OpportunityID")
+		}
+
+		f1, err := decodeNextGrantRecord(r)
+		assert.NoError(t, err)
+		if assert.IsType(t, forecast{}, f1) {
+			assert.Equal(t, "56789", string(f1.(forecast).OpportunityID),
+				"forecast has unexpected OpportunityID")
+		}
+
+		f2, err := decodeNextGrantRecord(r)
+		assert.NoError(t, err)
+		if assert.IsType(t, forecast{}, f2) {
+			assert.Equal(t, "13579", string(f2.(forecast).OpportunityID),
+				"forecast has unexpected OpportunityID")
+		}
+
+		o2, err := decodeNextGrantRecord(r)
+		assert.NoError(t, err)
+		if assert.IsType(t, opportunity{}, o2) {
+			assert.Equal(t, "24680", string(o2.(opportunity).OpportunityID),
+				"opportunity has unexpected OpportunityID")
+		}
+
+		finalRV, err := decodeNextGrantRecord(r)
+		assert.Nil(t, finalRV)
+		assert.ErrorIs(t, err, io.EOF)
+	})
+}
+
+func TestProcessGrantRecord(t *testing.T) {
 	now := time.Now()
 	testOpportunity := opportunity{
 		OpportunityID:   "123456",
@@ -212,7 +451,7 @@ func TestProcessOpportunity(t *testing.T) {
 				return nil, fmt.Errorf("some UpdateItem error")
 			}),
 		}
-		err := processOpportunity(context.TODO(), dynamodbClient, testOpportunity)
+		err := processGrantRecord(context.TODO(), dynamodbClient, testOpportunity)
 		assert.ErrorContains(t, err, "Error uploading prepared grant opportunity to DynamoDB")
 	})
 
@@ -227,6 +466,6 @@ func TestProcessOpportunity(t *testing.T) {
 				return nil, err
 			}),
 		}
-		assert.NoError(t, processOpportunity(context.TODO(), dynamodbClient, testOpportunity))
+		assert.NoError(t, processGrantRecord(context.TODO(), dynamodbClient, testOpportunity))
 	})
 }
